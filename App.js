f0a5b272dee881a9767609becc9e3bc512fa622e
PranaliPatil09 <57919083+PranaliPatil09@users.noreply.github.com>
2022-10-09 04:22:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+import {View,Text,StyleSheet} from "react-native";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component{
+  render(){
+    return <BottomTabNavigator/>
+  }
+}
